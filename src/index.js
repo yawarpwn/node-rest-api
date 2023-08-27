720e66080254274d279689fb41f0dc06def08c8d
yawarpwn <john.kazz@gmail.com>
2023-08-27 16:27:02 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const express = require("express");
+import express  from 'express'
+import cors from 'cors'
+
 const app = express();
+
+app.use(cors())
+console.log('start')
+
+app.get('/', (req, res) => {
+  res.status(200).end('<h1>Node App</h1>')
+})
 
 // This line is important to ensure your app listens to the PORT env var
 const port = process.env.PORT ?? 8080;
