f312c7d9f2db6b6f24dd026efe90d43af6ef4c68
yawarpwn <john.kazz@gmail.com>
2023-09-13 09:51:17 -0500

    try connection

--- src/models/signals.js
+++ src/models/signals.js
@@ -2,12 +2,30 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL)
+// database: tellsenales_database
+// username: c9qvpgzukugfrykw3bfk
+// host: aws.connect.psdb.cloud
+// password: pscale_pw_zuPVP6Cpr9WLFkcjTAEV7RcCLQuvzpRNcF1dEx3pN1u
+//
+const DEFAULT_CONNECTION = {
+  host: 'localhost',
+  user: 'root',
+  database: 'test.db',
+}
+const DB_CONNECTION = {
+  host: 'aws.connect.psdb.cloud',
+  user: 'c9qvpgzukugfrykw3bfk',
+  database: 'tellsenales_database',
+  password: 'pscale_pw_zuPVP6Cpr9WLFkcjTAEV7RcCLQuvzpRNcF1dEx3pN1u',
+}
 
+const connection = await mysql.createConnection(process.env.DATABASE_URL)
+// const connection =  mysql.createPool(DB_CONNECTION)
 
 export class SignalModel {
   static async getAll() {
-    const [rows, fields] = await connection.execute(`SELECT * FROM test`)
+    const [rows, fields] = await connection.query(`SELECT * FROM test`)
+    console.log(rows)
     return {
       data: rows,
       error: null,
